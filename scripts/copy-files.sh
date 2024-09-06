291283c26fa5edd2d74019d2301417bb4722fc47
dudushy <eduardotalarico1337@gmail.com>
2024-09-05 21:31:36 -0300

    ci: Remove old assets folder system

--- scripts/copy-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure the docs/assets/shyle directory exists
-mkdir -p docs/assets/shyle
-
-# Copy the compiled files to the docs directory
-cp dist/css/shyle.css docs/assets/shyle/css/shyle.css
-cp dist/js/shyle.js docs/assets/shyle/js/shyle.js
